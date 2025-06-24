6c2aad6c3ce2b6ac01289d5964eb4b996932dcd7
김민재 <kbckmj2050@gmail.com>
2025-06-24 02:13:34 -0400

    feat: attackcompo

--- Assets/01.Scipt/Item/Fireball.cs
+++ Assets/01.Scipt/Item/Fireball.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    private Rigidbody _rbCompo;
-    private void Start()
-    {
-        _rbCompo = GetComponent<Rigidbody>();
-        _rbCompo.AddForce(transform.forward * 8, ForceMode.Impulse);
-        Destroy(this, 5f);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
+        private Rigidbody _rbCompo;
+        private void Start()
         {
-            other.GetComponentInChildren<EntityHealth>().ApplyDamage(2,transform.position,null, null);
-            gameObject.SetActive(false);
-            CameraShakingManager.instance.ShakeCam(0.2f,0.2f,3,20);
+            _rbCompo = GetComponent<Rigidbody>();
+            _rbCompo.AddForce(transform.forward * 8, ForceMode.Impulse);
+            Destroy(this, 5f);
         }
-    }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.GetComponentInChildren<EntityHealth>().ApplyDamage(2,transform.position,null, null);
+                gameObject.SetActive(false);
+                CameraShakingManager.instance.ShakeCam(0.2f,0.2f,3,20);
+            }
+        }
 }
